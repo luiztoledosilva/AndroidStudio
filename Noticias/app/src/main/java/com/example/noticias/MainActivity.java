8fed9ba78b95cf152df28c2625fc9473e4d84275
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-09-17 16:02:06 -0300

    checkbox parte2

--- Noticias/app/src/main/java/com/example/noticias/MainActivity.java
+++ Noticias/app/src/main/java/com/example/noticias/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.noticias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CheckBox checkEconomia, checkEsporte, checkLazer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkEconomia = findViewById(R.id.checkEconomia);
+        checkEsporte = findViewById(R.id.checkEsporte);
+        checkLazer = findViewById(R.id.checkLazer);
+
+
+
+    }
+
+    public void checkbox(){
+
+        
+    }
+    public void enviar(View view){
+        checkbox();
     }
 }
