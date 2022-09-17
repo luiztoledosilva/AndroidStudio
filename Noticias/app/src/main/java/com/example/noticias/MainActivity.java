486a6da073ab93ec447b3e9529caa5a20dfe77ef
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-09-17 17:22:33 -0300

    checkbox parte3 checked economia teste

--- Noticias/app/src/main/java/com/example/noticias/MainActivity.java
+++ Noticias/app/src/main/java/com/example/noticias/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox checkEconomia, checkEsporte, checkLazer;
+    private TextView textResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         checkEsporte = findViewById(R.id.checkEsporte);
         checkLazer = findViewById(R.id.checkLazer);
 
-
+        textResultado = findViewById(R.id.textResultado);
+       
 
     }
 
     public void checkbox(){
-
+        boolean teste = checkEconomia.isChecked();
+        textResultado.setText("Economia pressionado: " +teste);
+         
         
     }
     public void enviar(View view){
